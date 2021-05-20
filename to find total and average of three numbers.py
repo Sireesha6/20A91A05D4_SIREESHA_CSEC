90f54e9537a69c0da75bbecaae08f924e1b9756f
Sireesha6 <84307474+Sireesha6@users.noreply.github.com>
2021-05-20 10:43:35 +0530

    Add files via upload

---
+++ to find total and average of three numbers.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+c=int(input("Enter c value"))
+total=a+b+c
+print("total=",total)
+average=(a+b+c)/3
+print("average=",average)
+#Enter a value4
+#Enter b value5
+#Enter c value6
+#total= 15
+#average= 5.0
