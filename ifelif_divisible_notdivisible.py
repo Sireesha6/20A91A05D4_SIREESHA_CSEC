ace315b4ada206d8e444771f44349dcc28727263
Sireesha6 <84307474+Sireesha6@users.noreply.github.com>
2021-05-25 14:27:27 +0530

    Add files via upload

---
+++ ifelif_divisible_notdivisible.py
@@ -0,0 +1,29 @@
+no=int(input("Enter number"))
+#Divisible by 2
+if no%2==0: #true
+    print ("Given no is divisible by 2")
+#If above cond is failed then control will check below
+elif no%3==0:
+    print("Divisible by 3")
+elif no%5==0:
+    print("Divisible by 5")
+else:
+    print("Not divisible by 2 or 3 or 5")
+print("Program Completed")
+
+
+#Enter number56
+#Given no is divisible by 2
+#Program Completed
+
+#Enter number27
+#Divisible by 3
+#Program Completed
+
+#Enter number25
+#Divisible by 5
+#Program Completed
+
+#Enter number 77
+#Not divisible by 2 or 3 or 5
+#Program Completed
