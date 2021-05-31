c77a57fc5f0fb7cd249e7d1deffde65a699f6f61
Sireesha6 <84307474+Sireesha6@users.noreply.github.com>
2021-05-31 12:31:27 +0530

    Add files via upload

---
+++ number_from_1to100_endedwith5.py
@@ -0,0 +1,23 @@
+i=5
+while(i<100):
+    print(i)
+    i=i+10
+else:
+    print("\n")
+    print("At %d value the loop is terminated" %i)
+
+'''
+Output :::
+5
+15
+25
+35
+45
+55
+65
+75
+85
+95
+
+
+At 105 value the loop is terminated'''
