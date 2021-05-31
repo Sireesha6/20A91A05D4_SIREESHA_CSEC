691c2d31cc120f7bf4f6c9697fa069d4c311a4dd
Sireesha6 <84307474+Sireesha6@users.noreply.github.com>
2021-05-31 12:39:59 +0530

    Add files via upload

---
+++ number_ended_with_3.py
@@ -0,0 +1,24 @@
+i=3
+while(i<100):
+    print(i)
+    i=i+10
+else:
+    print("\n")
+    print("At %d value the loop is terminated" %i)
+
+
+'''
+OUTPUT:::
+3
+13
+23
+33
+43
+53
+63
+73
+83
+93
+
+
+At 103 value the loop is terminated'''
