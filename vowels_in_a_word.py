dd606441789a163db753d05c496a5378a0bc0610
Sireesha6 <84307474+Sireesha6@users.noreply.github.com>
2021-06-25 12:00:21 +0530

    Add files via upload

---
+++ vowels_in_a_word.py
@@ -0,0 +1,14 @@
+s=input("Enter a word")
+print("vowels in the word are")
+for i in  s: #
+    if (i=='A' or i=='E' or i=='I' or i=='O' or i=='U' or i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+        print(i)
+        
+    
+'''
+Enter a wordsiREEshA
+vowels in the word are
+i
+E
+E
+A'''
